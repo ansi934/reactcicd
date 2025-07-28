a4d9084d3576869be36ca0295bcc52c3cba64207
ansi934 <2300032677@kluniversity.in>
2025-07-28 10:00:04 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+<img src="/vite.svg" alt="Vite Logo" />
+
 import './App.css'
 
 function App() {
