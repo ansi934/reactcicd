54d9ad1f4366da16bd6ed8f19c150155b7eb19d2
ansi934 <2300032677@kluniversity.in>
2025-07-29 11:33:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>this is github actions using CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
